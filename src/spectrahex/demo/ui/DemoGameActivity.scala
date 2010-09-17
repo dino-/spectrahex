309a1b8992bcdd87cdf18d2c2c1bc7e971444a3c
Dino Morelli <dino@ui3.info>
2010-09-17 16:00:50 +0000

    No longer popping up About dialog when DEMO starts

--- src/spectrahex/demo/ui/DemoGameActivity.scala
+++ src/spectrahex/demo/ui/DemoGameActivity.scala
@@ -13,8 +13,6 @@ class DemoGameActivity extends GameActivity {
 
    override def onCreate (savedInstanceState: Bundle) {
       super.onCreate (savedInstanceState)
-
-      showDialog(GameActivity.DIALOG_ABOUT)
    }
 
 
